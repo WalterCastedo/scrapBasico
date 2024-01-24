6d251effd6ddb44a437d97d0eaedb5852b508af6
WalterCastedo <86216014+WalterCastedo@users.noreply.github.com>
2024-01-24 18:24:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const cheerio = require('cheerio');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/',(req,res)=>{
   res.send('bienvenido')
 })
