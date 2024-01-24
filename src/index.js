72099954525e47d23d204afde968554dda9d60f4
WalterCastedo <86216014+WalterCastedo@users.noreply.github.com>
2024-01-24 18:36:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
