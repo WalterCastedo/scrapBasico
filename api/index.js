262b19759eb3272c371d287a6ee228f953a7d906
WalterCastedo <waltercastedosaucedo@gmail.com>
2024-01-24 16:07:19 -0400

    nuevo

--- api/index.js
+++ api/index.js
@@ -1,47 +1,36 @@
-const express = require('express');
+// index.js
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/scrape', async (req, res) => {
+module.exports = async (req, res) => {
   try {
-    // Lee el parámetro 'q' de la URL
-    const query = req.query.q; // Valor predeterminado si no se proporciona el parámetro 'q'
+    const query = req.query.q || ''; // Valor predeterminado si no se proporciona el parámetro 'q'
 
-    // Construye la URL con la consulta
     const url = `https://www.fidalga.com/search?q=${encodeURIComponent(query)}&type=product`;
 
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
-    // Array para almacenar la información de los productos
     const products = [];
 
-    // Selecciona todos los elementos con la clase 'products-grid'
     $('.products-grid .grid-item.col-6.col-md-4.grid-item-border.column-4').each((index, element) => {
       const productName = $(element).find('.product-title span').text().trim();
       const productPriceText = $(element).find('.price-regular span').text().trim().replace('Bs', '').replace(',', '.');
       const productPrice = parseFloat(productPriceText) || 0;
-      const productLink = $(element).find('.product-image a').attr('href'); // Cambio aquí
+      const productLink = $(element).find('.product-image a').attr('href');
 
       products.push({
         name: productName,
         price: productPrice,
         link: productLink,
-        order: index + 1, // El orden es el índice + 1
+        order: index + 1,
       });
     });
 
-    // Devuelve la lista de productos como respuesta JSON
     res.json(products);
   } catch (error) {
     console.error('Error al hacer la solicitud:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+};
